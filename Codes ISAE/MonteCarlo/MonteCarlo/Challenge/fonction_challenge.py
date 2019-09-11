8c95c7bc9bf5c2aaab063eb3a0f1ecebcdfec70b
Eduardo Dadalto <edadaltocg@gmail.com>
2019-09-11 21:17:41 +0200

    Igonring some files

--- Codes ISAE/MonteCarlo/MonteCarlo/Challenge/fonction_challenge.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-from scipy import stats
-
-def phi(x):
-    g=np.sqrt(2)/2*(3/4*stats.multivariate_normal.pdf(x,[0,np.pi,0,0,np.pi,0,0,0,0,0]) + 1/4*stats.multivariate_normal.pdf(x,[0,np.pi,0,0,-np.pi,0,0,0,0,0]))
-    return g
